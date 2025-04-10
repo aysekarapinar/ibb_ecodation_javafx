7d66bfff7170bef8c4629eb04cba548fb3d2575c
Ayşe Karapinar <ayseozlemkarapinar28@outlook.com>
2025-04-10 23:19:53 +0300

    JavaFX Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.aysekarapinar.ibb_ecodation_javafx2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.aysekarapinar.ibb_ecodation_javafx2 to javafx.fxml;
+    exports com.aysekarapinar.ibb_ecodation_javafx2;
+}
